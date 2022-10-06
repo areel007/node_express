18cba395a8b77a6acb524c389149659679f4c52b
Sunday Morenikeji <65197762+areel007@users.noreply.github.com>
2022-10-06 15:18:56 +0100

    testing routes

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express()
 
-app.use('/', (req, res) => res.end('Hello world!'))
+app.get('/', (req, res) => res.end('Hello world!'))
+
+app.get(/.*fly$/, (req, res) => {
+  res.send('/.*fly$/')
+})
 
 app.listen(3000, () => console.log('App listening on port 3000...'))
